9927c5a05f611df9f12e5380b74081fb4df74731
MyznEiji <em711th@icloud.com>
2017-12-15 14:20:13 +0900

    Update checkTheWebsite.py

---
+++ toDoList/checkTheWebsite.py
@@ -0,0 +1,12 @@
+import webbrowser
+
+
+webbrowser.open("https://qiita.com/trend")
+webbrowser.open("http://gigazine.net/")
+webbrowser.open("http://www.sensors.jp/")
+webbrowser.open("http://www.itmedia.co.jp/")
+webbrowser.open("https://wired.jp/")
+webbrowser.open("https://bouncy.news/")
+webbrowser.open("https://www.lifehacker.jp/")
+webbrowser.open("http://shiropen.com/")
+webbrowser.open("https://newspicks.com/")
